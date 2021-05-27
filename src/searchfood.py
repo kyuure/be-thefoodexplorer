650a844846634d39eb8bedf2dedfc9dd91266274
S Qothrunnada <qothrunnada.salsa@gmail.com>
2021-05-27 16:11:44 +0700

    Clean code
    Cleaning code so that the code readable.

--- src/searchfood.py
+++ src/searchfood.py
@@ -1,4 +1,5 @@
 # Source: https://cloud.google.com/firestore/docs/quickstart-servers#python
+from re import search
 from google.cloud import firestore
 db = firestore.Client()
 
@@ -51,59 +52,32 @@ def searchFoodByText(query):
     GET method
     pake query
     """
-    # Initialize data
-    data = {}
 
+    # Do querying
     users_ref = db.collection(u'food')
     docs = users_ref.stream()
-    '''
-    for doc in docs:
-        data = doc.to_dict()
-    '''    
-   
-
-
-
-    # Do querying and check for the result
-    # Currently using dummy data
-   
-    result = {
-            'success' : True,
-            'message' : 'Some message',
-            'data' : [
-                {
-                    'id': 0,
-                    'name': 'Tahu',
-                    'city': 'Jakarta',
-                    'image': 'localhost'
-                },
-                {
-                    'id': 0,
-                    'name': 'Tahu',
-                    'city': 'Jakarta',
-                    'image': 'localhost'
-                }
-            ]
-        }
     
-    data = { el.id: el.to_dict() for el in docs }
-    real_data = data[query]
-    send_dict = {}
-    send_dict['id'] = real_data['id']
-    send_dict['name'] = query
-    send_dict['city'] = real_data['city'] 
-    send_dict['image'] = real_data['image']
-    
+    # Assign the query to local variable
+    real_dict = {}
+    send_dict = []
+    for el in docs:
+        if not search('*'+query+'*', str(el.id)):
+            continue
+        data = el.to_dict()
+        real_dict['id']    = data['id']
+        real_dict['name']  = el.id
+        real_dict['city']  = data['city'] 
+        real_dict['image'] = data['image']
+        send_dict.append(real_dict)
 
-    if not result:
+    # Check for the result
+    if not send_dict:
         return {'success' : False,
                 'message' : f'Query {query} doesnt match any object.'}
-        
-    return {'success' : True,
-                'message': 'BERHASIL BERHASIL BERHASIL HOREEEE',
-                'data': send_dict
-                }
 
-    #data = result
-    #data = doc.to_dict()
-    #return data
+    # Return the result
+    return {
+            'success' : True,
+            'message': 'BERHASIL BERHASIL BERHASIL HOREEEE',
+            'data': send_dict
+        }
