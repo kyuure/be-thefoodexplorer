61cb92174bf9a04c0b2b43c18b1f7666e732a9e5
S Qothrunnada <qothrunnada.salsa@gmail.com>
2021-05-28 11:53:34 +0700

    Change string to HTML format

--- src/help.py
+++ src/help.py
@@ -1,34 +1,29 @@
 def print_welcome():
     return """
 <h1>Hello world!</h1>
-
-<h2>This API is intended for TheFoodExplorer application.</h2>
+<p>
+<h3>This API is intended for FoodExplorer application.</h2>
 
 """
 
 
 def print_man():
     return """
-    <h3>api/search/image</h3>
+    <h3>api/v1/food</h3>
       <p>POST method</p>
       <p>form data berisi image</p>
 
-    <h3>api/search/<name></h3>
+    <h3>api/v1/food?q={query}</h3>
       <p>GET method</p>
       <p>search food using text</p>
       <p>nyari pake query</p>
 
-    <h3>api/food</h3>
-      <p>GET method</p>
-      <p>get all food data</p>
-      <p>query semua (SELECT *)</p>
-
-    <h3>api/food/<int:id></h3>
+    <h3>api/v1/food/<int:food_id>/detail</h3>
       <p>GET method</p>
       <p>get food details based on id</p>
       <p>pake path berupa id-nya food</p>
 
-    <h3>api/food/store/<int:id></h3>
+    <h3>api/v1/food/<int:food_id>/location</h3>
       <p>GET method</p>
       <p>get food stores based on id</p>
       <p>pake path berupa id-nya food</p>
