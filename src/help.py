b7fb3ba2ab6c42003a81117c7b6ecfeff464b4a6
S Qothrunnada <qothrunnada.salsa@gmail.com>
2021-06-01 13:17:17 +0700

    Add documentation

--- src/help.py
+++ src/help.py
@@ -18,12 +18,17 @@ def print_man():
       <p>search food using text</p>
       <p>nyari pake query</p>
 
+    <h3>api/v1/food/all</h3>
+      <p>GET method</p>
+      <p>get all food in the database</p>
+      <p>ambil semua data</p>
+
     <h3>api/v1/food/<int:food_id>/detail</h3>
       <p>GET method</p>
       <p>get food details based on id</p>
       <p>pake path berupa id-nya food</p>
 
-    <h3>api/v1/food/<int:food_id>/location</h3>
+    <h3>api/v1/food/<int:food_id>/location?latitude={latitude}&longitude={longitude}</h3>
       <p>GET method</p>
       <p>get food stores based on id</p>
       <p>pake path berupa id-nya food</p>
